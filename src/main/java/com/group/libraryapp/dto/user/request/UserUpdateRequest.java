a783d21ad8a9b01cc8689cdb4c06f0ae96a831f3
daylee619 <110576332+daylee619@users.noreply.github.com>
2023-11-23 21:02:04 +0900

    commit

---
+++ src/main/java/com/group/libraryapp/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.group.libraryapp.dto.user.request;
+
+public class UserUpdateRequest {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
